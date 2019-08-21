41b862ae4d8cb8ec75e334a2615e2f6e298181e0
unknown <lunakadysz@gmail.com>
2019-08-21 12:56:06 -0300

    laburo

--- ajuste.py
+++ ajuste.py
@@ -9,16 +9,6 @@ yerrado = data[:,2]
 error_y = data[:,3]
 
 
-f = lambda x, A, B: A * x + B
-
-# Ajustamos, pero con las funciónes logaritmicas. Usamos propagación de errores
-popt, pcov = curve_fit(f, np.log(x), np.log(y), sigma = 1/y * error_y, 
-                       absolute_sigma=True)
-
-print(popt)
-print(pcov)
-
-
 
 # Detalles del gráfico
 plt.grid(True) # Para que quede en hoja cuadriculada
@@ -26,8 +16,8 @@ plt.title('Grafico ejemplo')
 plt.xlabel('Valores en x')
 plt.ylabel('Valores en y')
 plt.legend(loc = 'best') 
-plt.yscale('sqrt')
-plt.xscale('sqrt')
-plt.errorbar(x, yerrado, yerr=error_y, fmt='go', label="Datos")
+plt.yscale('log')
+plt.xscale('log')
+plt.errorbar(x, yerrado, yerr=error_y, fmt='go', label="Datos", ecolor = 'red')
 plt.show() 
 
--- graphs.py
+++ graphs.py
@@ -9,7 +9,7 @@ plt.plot(x, y, 'ro')
 plt.axis([0, 6, -20, 26])
 
 def f(t):
-    return 40 * np.exp(-t) * np.cos(8*np.pi*t)
+    return 40 * np.exp(-t) 
 plt.plot(x, f(x), 'b')
 
 plt.show()
@@ -18,13 +18,13 @@ sigma = 0.8
 ruido=[]
 ruidoo=[]
 # creating a noise with the same dimension as the dataset (2,2) 
-for i in np.nditer(y):
+for i in np.nditer(f(x)):
     mu = i
     ruido1 = np.random.normal(mu, sigma,1) 
-    ruido2 = np.random.exponential(1.0,1)
+    ruido2 = np.random.normal(0,2,1)
     ruido.append(ruido1)
     ruidoo.append(ruido2)
 #np.savetxt('doble_exp.dat', (x,y),delimiter="\n", newline='\r\n')
-print(ruido,ruidoo)
+print(ruidoo)
 
-np.savetxt('doble_exp.dat',np.transpose([x,y,ruido,ruidoo]))   
+np.savetxt('doble_exp.dat',np.transpose([x,f(x),ruido,ruidoo]))   
