2fc254101953d4d053b938e1f14df94b704ef0f5
unknown <lunakadysz@gmail.com>
2019-10-10 12:50:09 -0300

    data

--- Labo3/2do Informe/pasaBajosL.py
+++ Labo3/2do Informe/pasaBajosL.py
@@ -10,7 +10,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
  
-frec = np.array([10,50,100,160,200,220,240,260,280,300,320,340,360,380,400,420,450,500,550,600,700,800,900,1000,1250])
+frec = np.array([10,50,100,160,200,220,240,260,280,300,320,340,360,380,400,420,450,500,550,600,700,800,900,1000,1250])*2*np.pi
 V_ent = np.array(len(frec)*[9.92])
 V_sal = np.array([8.24,8.16,7.96,7.68,7.44,7.29,7.12,6.96,6.8,6.64,6.44,6.28,6.16,6,5.84,5.68,5.48,5.16,4.88,4.64,4.08,3.72,3.44,3.12,2.56])
 
@@ -28,7 +28,7 @@ def f(frec,f0):
 param, param_cov = curve_fit(f,frec, V_sal/V_ent, p0=[320]) 
   
 print("f0") 
-print(param[0]) 
+print(param[0]/(2*np.pi)) 
  
 print("Covariance of coefficients:") 
 print(param_cov) 
@@ -36,6 +36,6 @@ print(param_cov)
 ans1 = f(frec,param[0])
 plt.errorbar(frec,V_sal/V_ent,yerr=propagacion, fmt='.',color ='black',label ="Datos", ecolor = 'red')
 plt.plot(frec, ans1, '--', color ='blue', label ="Ajuste") 
-plt.title('Pasa altos con Inductancia')
+plt.title('Pasa bajos con Inductancia')
 plt.xlabel('Frecuencia [Hz]')
 plt.ylabel('Transferencia ')
--- Labo3/Transitores/RL.py
+++ Labo3/Transitores/RL.py
@@ -12,7 +12,7 @@ import scipy
 
 # L = 1H
 # R = 4000 Ohms
-# frec = 300Hz
+# frec = 300Hz => periodo = 0.00333333333shttps://towardsdatascience.com/playing-with-time-series-data-in-python-959e2485bff8
 # Vpicopico = 5V con offset de 2.5V
 
 dato = scipy.io.loadmat('RL.m')
@@ -22,8 +22,16 @@ tiempo = matriz[:,0]
 Vc = matriz[:,1]
 Vfuente = matriz[:,2]
 
+def f(t,V0,tau):
+    return V0 * np.e**(-t/tau)
+
+param, param_cov = curve_fit(f,tiempo,Vc, p0=[5,1]) 
+
+
 plt.figure()
+plt.plot(tiempo,f(tiempo,param[0],param[1]), '--', color ='green', label ="Ajuste") 
 plt.scatter(tiempo,Vc,s= 0.5)
-plt.scatter(tiempo,Vfuente,color='orange',s= 0.5)
-plt.xlim(-0.0018,0.005)
+#plt.scatter(tiempo,Vfuente,color='orange',s= 0.5)
+#plt.ylim(0,5)
 plt.show()
+
